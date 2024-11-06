548ed9caeb913f65ded08344438afc614746536c
David Sagan <dcs16@cornell.edu>
2024-11-06 14:24:31 -0500

    New particle_properties_conversion.jl.

--- src/SimUtils.jl
+++ src/SimUtils.jl
@@ -1,9 +1,10 @@
 module SimUtils
 using FFTW, Random, GTPSA
 
-export gen_pinknoise
 #export Quaternion
+export gen_pinknoise
 export cos_one, modulo2
+export E_tot, E_kinetic, pc, β, β1, γ
 
 include("math.jl")
 include("pinknoise.jl")
