71865c74c94659fe7e909d465be7d5190cc20e29
dtabell <dtabell@users.noreply.github.com>
2025-01-24 17:35:39 -0700

    Remove trailing blanks.

--- src/math.jl
+++ src/math.jl
@@ -68,8 +68,8 @@ end
 """
   coscu(x, nd::Int = 0)
 
-Returns, to machine precision, the `nd`th derivative of `(1 - cos(x)) / x^2`. 
-If `nd = 0`, returns the function itself. 
+Returns, to machine precision, the `nd`th derivative of `(1 - cos(x)) / x^2`.
+If `nd = 0`, returns the function itself.
 
 Note: Currently, only `nd = 0` or `1` are implemented
 """ coscu
