d1811bd272cc631b71439311d99a56c3e80a1574
David Sagan <dcs16@cornell.edu>
2025-02-02 00:55:23 -0500

    New sqrt_one function.

--- src/AcceleratorSimUtils.jl
+++ src/AcceleratorSimUtils.jl
@@ -6,7 +6,7 @@ using AtomicAndPhysicalConstants
 @APCdef
 
 export gen_pinknoise
-export one_cos, modulo2, sincu, coscu
+export one_cos, sqrt_one, modulo2, sincu, coscu
 export calc_E_tot, calc_E_kinetic, calc_pc, calc_β, calc_1β, calc_γ, calc_changed_energy
 
 include("math.jl")
